5cf39d1c7a8b570e6b4e17a812a0c6e9b01df7f4
c.klewar <c.klewar@f5.com>
2024-04-19 17:56:01 +0200

    rework azure module to support existing subnet v2

--- examples/single_node_single_nic_existing_vnet_existing_subnet/main.tf
+++ examples/single_node_single_nic_existing_vnet_existing_subnet/main.tf
@@ -7,28 +7,29 @@ locals {
 }
 
 resource "azurerm_resource_group" "f5xc_ce_single_node_multi_nic_existing_rg_existing_vnet" {
-  location = var.azurerm_region
   name     = format("%s-%s-%s", var.project_prefix, "azure-ce-exists-rg", var.project_suffix)
+  location = var.azurerm_region
   provider = azurerm.default
 }
 
 resource "azurerm_virtual_network" "f5xc_ce_single_node_multi_nic_existing_rg_existing_vnet" {
-  address_space       = var.azurerm_vnet_address_space
   name                = format("%s-%s-%s", var.project_prefix, "azure-ce-exists-vnet", var.project_suffix)
   provider            = azurerm.default
   location            = var.azurerm_region
+  address_space       = var.azurerm_vnet_address_space
   resource_group_name = azurerm_resource_group.f5xc_ce_single_node_multi_nic_existing_rg_existing_vnet.name
 }
 
 resource "azurerm_subnet" "slo" {
   name                 = "node0-subnet-slo"
+  provider             = azurerm.default
   address_prefixes     = [var.azurerm_vnet_subnet_node0_slo]
   resource_group_name  = azurerm_resource_group.f5xc_ce_single_node_multi_nic_existing_rg_existing_vnet.name
   virtual_network_name = azurerm_virtual_network.f5xc_ce_single_node_multi_nic_existing_rg_existing_vnet.name
 }
 
 module "f5xc_azure_cloud_ce_single_node_single_nic_existing_vnet_new_subnet" {
-  depends_on        = [azurerm_resource_group.f5xc_ce_single_node_multi_nic_existing_rg_existing_vnet, azurerm_virtual_network.f5xc_ce_single_node_multi_nic_existing_rg_existing_vnet]
+  depends_on        = [azurerm_resource_group.f5xc_ce_single_node_multi_nic_existing_rg_existing_vnet, azurerm_virtual_network.f5xc_ce_single_node_multi_nic_existing_rg_existing_vnet, azurerm_subnet.slo]
   source            = "../../modules/f5xc/ce/azure"
   owner_tag         = var.owner
   is_sensitive      = false
@@ -41,7 +42,7 @@ module "f5xc_azure_cloud_ce_single_node_single_nic_existing_vnet_new_subnet" {
   f5xc_azure_az_nodes = {
     node0 = {
       az                       = var.azurerm_az_node0
-      existing_subnet_name_slo = var.azurerm_existing_subnet_name_slo
+      existing_subnet_name_slo = azurerm_subnet.slo.name
     }
   }
   f5xc_token_name                         = format("%s-%s-%s", var.project_prefix, var.f5xc_cluster_name, var.project_suffix)
@@ -58,7 +59,6 @@ module "f5xc_azure_cloud_ce_single_node_single_nic_existing_vnet_new_subnet" {
   azurerm_marketplace_version             = "0.9.2"
   azure_security_group_rules_slo          = []
   azurerm_instance_admin_username         = var.azurerm_instance_admin_username
-  azurerm_existing_subnet_name_slo        = azurerm_subnet.slo.name
   azurerm_existing_resource_group_name    = azurerm_resource_group.f5xc_ce_single_node_multi_nic_existing_rg_existing_vnet.name
   azurerm_disable_password_authentication = var.azurerm_disable_password_authentication
   ssh_public_key                          = file(var.ssh_public_key_file)
